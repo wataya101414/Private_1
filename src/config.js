93690f9e4b2b6f510f510b033d8e51bd55515a46
wataya <wataya.natsuki@gmail.com>
2026-08-15 17:20:37 +0900

    jsファイルへバックエンド側のURLを記載

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-// Cloudflare Pagesへデプロイする前に、Renderの公開URLを設定してください。
 // 例: API_BASE_URL: "https://private-1-api.onrender.com"
 window.APP_CONFIG = Object.freeze({
-  API_BASE_URL: ""
+  API_BASE_URL: "https://private-1-918l.onrender.com/"
 });
